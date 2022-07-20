f7f8eb7e191eed3cfa225580a7acfaf977a28dea
Anup Haldar <haldaranup746@gmail.com>
2022-07-20 18:36:44 +0530

    add mid and bottom section

---
+++ src/Component/Partners.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "../Styles/partners.scss";
+const Partners = () => {
+  return (
+    <div className="brandsContainer">
+      <div className="brandsImg">
+        <img
+          src="https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-vw.svg?noresize"
+          alt=""
+        />
+        <img
+          src="https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-aclu.svg?noresize"
+          alt=""
+        />
+        <img
+          src="https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-conde.svg?noresize"
+          alt=""
+        />
+        <img
+          src="https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-dell.svg?noresize"
+          alt=""
+        />
+        <img
+          src="https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-amnesty.svg?noresize"
+          alt=""
+        />
+        <img
+          src="https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-deloitte.svg?noresize"
+          alt=""
+        />
+        <img
+          src="https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-lululemon.svg?noresize"
+          alt=""
+        />
+        <img
+          src="https://www.getharvest.com/hubfs/raw_assets/public/harvest-theme/images/customer-logos/featured-yale.svg?noresize"
+          alt=""
+        />
+      </div>
+      <div className="line"></div>
+    </div>
+  );
+};
+
+export default Partners;
--- src/Pages/WhyHarvestPage.jsx
+++ src/Pages/WhyHarvestPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import MidHarvest from '../Component/MidHarvest'
+import Partners from '../Component/Partners'
 import TrialCard from '../Component/TrialCard'
 
 const WhyHarvestPage = () => {
   return (
     <div>
         <TrialCard />
+        <MidHarvest />
+        <Partners />
     </div>
   )
 }
