c327e0a226bc7d16b587f2799c8c2ba1b2f17a43
Surajbnp <97525465+Surajbnp@users.noreply.github.com>
2022-07-21 11:04:26 +0530

    signup page initiated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,9 +10,6 @@ import {
 } from "@chakra-ui/react";
 import React from "react";
 import Footer from "../Component/Footer";
-
-import React from "react";
-
 import Navbar from "../Component/Navbar";
 import { StarIcon } from "@chakra-ui/icons";
 import { ImStarHalf } from "react-icons/im";
